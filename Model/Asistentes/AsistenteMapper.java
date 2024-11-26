1c8e15c6ca96da9bc36a3aa7aa48489615fe8153
Keizel <kemarchenabo@est.utn.ac.cr>
2024-11-26 10:37:03 -0600

    Clase AsistenteDTO

--- Model/Asistentes/AsistenteMapper.java
+++ Model/Asistentes/AsistenteMapper.java
@@ -4,10 +4,35 @@
  */
 package Model.Asistentes;
 
+import Mapper.Mapper;
+
 /**
  *
  * @author Student
  */
 public class AsistenteMapper implements Mapper<Asistente, AsistenteDTO> {
+
+    @Override
+    public AsistenteDTO toDto(Asistente ent) {
+                return new AsistenteDTO(
+                ent.getCedula(),
+                ent.getNombre(),
+                ent.getTelefono(),
+                ent.getCorreo()
+        );
+
+    }
+
+    @Override
+    public Asistente toEntity(AsistenteDTO dto) {
+        if(dto == null) return null;
+        return new Asistente(
+               dto.getCedula(),
+               dto.getNombre(),
+               dto.getTelefono(),
+               dto.getCorreo()
+       );
+
+    }
     
 }

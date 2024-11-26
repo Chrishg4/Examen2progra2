7e102441ddc8f55e31bb0b0df85bb26ac11994ef
Keizel <kemarchenabo@est.utn.ac.cr>
2024-11-26 10:21:43 -0600

    Clase AsistenteDAO, DTO Y Mapper

---
+++ Model/Asistentes/AsistenteDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Asistentes;
+
+/**
+ *
+ * @author Student
+ */
+public class AsistenteDTO {
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public AsistenteDTO(String cedula, String nombre, String telefono, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+}

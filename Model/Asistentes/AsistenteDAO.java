ca433e54fdb76ac9fa0f7f64b02bfac2304df090
Keizel <kemarchenabo@est.utn.ac.cr>
2024-11-26 11:30:59 -0600

    Programación AsistenteDAO

--- Model/Asistentes/AsistenteDAO.java
+++ Model/Asistentes/AsistenteDAO.java
@@ -7,6 +7,7 @@ package Model.Asistentes;
 import DAO.DaoCRUD;
 //import com.sun.jdi.connect.spi.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,13 +19,16 @@ public class AsistenteDAO extends DaoCRUD<AsistenteDTO> {
 
     @Override
     public boolean update(AsistenteDTO dto) throws SQLException {
-       
+        stmt = connection.prepareStatement("call AsistentesUpdate(?,?)");
+        stmt.setString(1, dto.getTelefono());
+        stmt.setString(2, dto.getCorreo());
+        return stmt.executeUpdate()>0;
 
     }
 
     @Override
     public boolean create(AsistenteDTO dto) throws SQLException {
-        stmt = connection.prepareStatement("call AsistenteCreate(?,?,?,?)");
+        stmt = connection.prepareStatement("call AsistentesCreate(?,?,?,?)");
         stmt.setString(1, dto.getCedula());
         stmt.setString(2, dto.getNombre());
         stmt.setString(3, dto.getTelefono());
@@ -35,16 +39,28 @@ public class AsistenteDAO extends DaoCRUD<AsistenteDTO> {
     @Override
     public AsistenteDTO read(Object id) throws SQLException {
         
+       
     }
 
     @Override
     public List<AsistenteDTO> readAll() throws SQLException {
-        
+        stmt = connection.prepareStatement("call AsistentesReadAll()");
+        rs = stmt.executeQuery();
+        List<AsistenteDTO> dtos = new ArrayList();
+        while (rs.next()) {
+            dtos.add(new AsistenteDTO(
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getDate(3),
+                    rs.getString(4),
+            ));
+        }
+        return dtos;
     }
 
     @Override
     public boolean delete(Object id) throws SQLException {
-        stmt = connection.prepareStatement("call AsistenteDelete(?)");
+        stmt = connection.prepareStatement("call AsistentesDelete(?)");
         stmt.setString(1, String.valueOf(id));
         return stmt.executeUpdate()>0;
     }
